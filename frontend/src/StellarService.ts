4b900fe01ae2a447e67f4c88b2ec87d18d468ab3
kalepail <hi@tyvdh.com>
2025-12-01 11:49:23 -0500

    Update StellarService.ts

--- frontend/src/StellarService.ts
+++ frontend/src/StellarService.ts
@@ -44,7 +44,7 @@ export class StellarService {
    * @param secretKey - Optional secret key for signing transactions (defaults to alice's key)
    */
   constructor(
-    contractId: string = 'CCGOTRYRB6R3E6OY2XQAVQGM2T3URVQERFXQGU7HJEMONY7DU3UUCPOQ',
+    contractId: string = 'CBKXARYKOS35FABGCETA5YBUKIIO45YOJDIKSOJNDN6K32TRCLDAWQ4G',
     rpcUrl: string = 'https://noir-local.stellar.buzz/soroban/rpc',
     secretKey?: string
   ) {
